d84ab57727e6d9a41536e589a073ccbef111483e
litencatt <ncl0709@gmail.com>
2023-02-25 10:10:26 +0900

    Add cookie session for gin

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"playground-gin/handler"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	store := cookie.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("sample_session", store))
+
 	r.LoadHTMLGlob("templates/*.go.tmpl")
 	r.GET("/", handler.RootHandler)
 	r.GET("/foo", handler.FooHandler)
