925ffdb81bd8d066a41ec23ff3eba4df8e0dc825
litencatt <ncl0709@gmail.com>
2023-02-25 10:13:28 +0900

    Save session and get session value sample

--- handler/handler.go
+++ handler/handler.go
@@ -3,17 +3,24 @@ package handler
 import (
 	"net/http"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 func RootHandler(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Set("session_value", 1)
+	session.Save()
 	c.HTML(http.StatusOK, "root.go.tmpl", gin.H{
 		"title": "root",
 	})
 }
 
 func FooHandler(c *gin.Context) {
+	session := sessions.Default(c)
+	val := session.Get("session_value")
 	c.HTML(http.StatusOK, "foo.go.tmpl", gin.H{
 		"title": "foo",
+		"value": val,
 	})
 }
